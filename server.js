3687da7f206ef075b8496e6544c98b6484933215
GimAndTonic <gugel.tim@gmail.com>
2024-06-15 13:16:36 +0200

    Support searching for notes in local dir and

--- server.js
+++ server.js
@@ -8,15 +8,29 @@ const port = 3000;
 // Serve the static HTML file
 app.use(express.static(path.join(__dirname)));
 
-// Endpoint to list files in the current directory
-app.get('/list-files', (req, res) => {
-    fs.readdir(__dirname, (err, files) => {
-        if (err) {
-            res.status(500).json({ error: 'Unable to list files' });
-        } else {
-            res.json(files.filter(file => file.endsWith('.html') || file.endsWith('.md')));
-        }
+// Function to list files in a directory
+function listFilesInDirectory(directory) {
+    return new Promise((resolve, reject) => {
+        fs.readdir(directory, (err, files) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(files.map(file => path.join(directory, file)));
+            }
+        });
     });
+}
+
+// Endpoint to list files in the current directory and the notes directory
+app.get('/list-files', async (req, res) => {
+    try {
+        const currentDirFiles = await listFilesInDirectory(__dirname);
+        const notesDirFiles = await listFilesInDirectory(path.join(__dirname, 'notes'));
+        const allFiles = currentDirFiles.concat(notesDirFiles).map(file => path.relative(__dirname, file));
+        res.json(allFiles.filter(file => file.endsWith('.html') || file.endsWith('.md')));
+    } catch (err) {
+        res.status(500).json({ error: 'Unable to list files' });
+    }
 });
 
 // Serve the index.html file at the root URL
